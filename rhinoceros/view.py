4a33af478f7973b07cf62581b69ab7cc55d296d3
Maksim <Jenny6199@yandex.ru>
2023-01-03 19:47:59 +0300

    prepere rinoseros/view.py

---
+++ rhinoceros/view.py
@@ -0,0 +1,21 @@
+class View:
+
+    def get(self, request, *args, **kwargs):
+        """
+        method for GET-request
+        :param request:
+        :param args:
+        :param kwargs:
+        :return:
+        """
+        pass
+
+    def post(self, request, *args, **kwargs):
+        """
+        method for POST-request
+        :param request:
+        :param args:
+        :param kwargs:
+        :return:
+        """
+        pass
