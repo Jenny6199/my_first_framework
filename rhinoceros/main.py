2525a79fbe134196d2c5aa6af580715625d0ac24
Maksim <Jenny6199@yandex.ru>
2023-01-05 20:03:32 +0300

    some documentation

--- rhinoceros/main.py
+++ rhinoceros/main.py
@@ -1,6 +1,6 @@
 import re
 from typing import List, Type
-from rhinoceros.exceptions import NotAllowed
+from rhinoceros.exceptions import NotAllowed, NotFound
 from rhinoceros.urls import Url
 from rhinoceros.view import View
 from rhinoceros.request import Request
@@ -12,15 +12,22 @@ class Rhinoceros:
     __slots__ = ('urls',)
 
     def __init__(self, urls: List[Url]):
+        """
+        Конструктор класса Rhinoceros
+        :param urls: List[Url]
+        """
         self.urls = urls
 
     def __call__(self, environ, start_response):
-        raw_url = environ['PATH_INFO']
-        view = self._find_view(raw_url)()
-        method = environ['REQUEST_METHOD'].lower()
-        if not hasattr(view, method):
-            raise NotAllowed
-        raw_response = getattr(view, method)(None)
+        """
+        Функция вызываемая при обращении к классу
+        :param environ: список переменных окружения
+        :param start_response: начальный ответ
+        :return: iter([response])
+        """
+        view = self._get_view(environ)
+        request = self._get_request(environ)
+        raw_response = self._get_response(environ, view, request)
         response = raw_response.encode('utf-8')
         start_response('200 OK', [
             ('Content-Type', 'text/plain'),
@@ -39,16 +46,45 @@ class Rhinoceros:
         return url
 
     def _find_view(self, raw_url: str) -> Type[View]:
+        """
+        Method for find match view for definitions in framework
+        :param raw_url: string with query params
+        :return: path.view
+        """
         url = self._prepare_url(raw_url)
         for path in self.urls:
             match = re.match(path.url, url)
             if match is not None:
                 return path.view
+        raise NotFound
 
     def _get_view(self, environ) -> View:
+        """
+        Method return view that it is equvivalen for query
+        :param environ: environment
+        :return: view
+        """
         raw_url = environ['PATH_INFO']
         view = self._find_view(raw_url)()
         return view
 
     def _get_request(self, environ: dict):
+        """
+        Обрабатывает полученный запрос
+        :param environ: переменные окружения
+        :return: Request(environ)
+        """
         return Request(environ)
+
+    def _get_response(self, environ: dict, view: View, request: Request):
+        """
+        Метод подготавливает ответ
+        :param environ: переменные окружения
+        :param view: объект класса View
+        :param request: объект класса Request
+        :return: getattr(view, method)(None)
+        """
+        method = environ['REQUEST_METHOD'].lower()
+        if not hasattr(view, method):
+            raise NotAllowed
+        return getattr(view, method)(None)
